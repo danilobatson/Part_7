24228e5a9d32c8aab07a780ac66b326a31cb1683
danilobatson <djbatson19@gmail.com>
2022-11-29 16:20:20 -0800

    complete

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Footer from './Footer';
-import About from './About';
-import Menu from './Menu';
-import CreateNew from './CreateNew';
-import AnecdoteList from './AnecdoteList';
-import Anecdote from './Anecdote';
-
-export { Footer, About, Menu, CreateNew, AnecdoteList, Anecdote };
